93fb8faff240d99b0174e670ff28cfcb83c28a62
AlbertoRanno <albertoranno@hotmail.com>
2022-11-03 14:16:28 -0300

    HTML

--- omitir/apuntesABorrar.js
+++ omitir/apuntesABorrar.js
@@ -3,6 +3,7 @@
 
 // npm init - package.json - dependencias/modulos
 // npm install express - requerirlo - guardar su ejecución en una constante "app"
+//Escritura de manejadores de peticiones con diferentes verbos HTTP en diferentes caminos URL (rutas)
 // creo datos como un objeto con propiedades clave-valor, y lo exporto - lo requiero en app.js
 
 const PUERTO = process.env.PORT || 3000;
@@ -20,16 +21,17 @@ app.get("/api/datos", (req, res) => {res.send(datos)})
 //Parametros URL "/:id"
 app.get("/api/datos/personal/:id", (req, res) => {
   // lo extraigo del request con:
-  const id = req.params.id
-  const resultados = datos.personal.filter(persona => persona.id == id)
+  const id = req.params.id;
+  const resultados = datos.personal.filter((persona) => persona.id == id);
+  // Obs! Ojo que acá no entendía el error, y era por como tenía los datos, y con === no funcionaba
+  // (string vs number)
 
-  if(resultados.length === 0){
-    return res.status(404).send(`No se encontro a nadie con el id ${id}`)
+  if (resultados.length === 0) {
+    return res.status(404).send(`No se encontro a nadie con el id ${id}`);
   }
   res.send(resultados);
 });
-// Obs! Ojo que acá no entendía el error, y era por como tenía los datos, y con === no funcionaba
-// (string vs number)
+
 
 //Doble Parámetro URL - Chequear, no funciona:
 routerPersonas.get("/:rol/:seniority", (req, res) => {
@@ -46,7 +48,7 @@ routerPersonas.get("/:rol/:seniority", (req, res) => {
 })
 
 //codigos de estado
-// res.status(404) porque el recurso no fue encontrado .send...
+// "res.status(404)" porque el recurso no fue encontrado ".send"...
 // status(200) viene por defecto, significa todo ok
 
 //Parametros Query
@@ -54,23 +56,22 @@ routerPersonas.get("/:rol/:seniority", (req, res) => {
 // ej... ?ordenar=id
 // si en esa ruta, pongo: console.log(req.query.ordenar); en consola veré "id"
 
-// Sirven para ordenar los resultados en un orden especifico:
+// Sirven, entre otros, para ordenar los resultados en un orden especifico:
 
 app.get("/api/datos/personal", (req, res) => {
-console.log(req.query.ordenar);
-    if (req.query.ordenar == "id") {
-      return res.send(
-        datos.personal.sort((a, b) => b.id - a.id)
-      );
-      //el método sort, permite ordenar una lista, en base a un criterio, determinado por la funcion
-      //en este caso, se ordena uno u otro primero, si el signo de eso da + o - (cambiando por a.id - b.id)
-    }
+  //http://localhost:3000/api/datos/personal?ordenar=id
+  console.log(req.query.ordenar);
+  if (req.query.ordenar == "id") {
+    return res.send(datos.personal.sort((a, b) => b.id - a.id));
+    //el método sort, permite ordenar una lista, en base a un criterio, determinado por la funcion
+    //en este caso, se ordena uno u otro primero, si el signo de eso da + o - (cambiando por a.id - b.id)
+  }
   res.send(datos.personal);
 });
 
 // Routers
 // Sirven para simplificar lo confuso que pueden llegar a ser varias rutas largas: /api/datos/proyectos
-const routerProyectos =express.Router()
+const routerProyectos = express.Router()
 app.use("/api/datos/proyectos", routerProyectos);
 // Reemplazan: app.get("/api/datos/proyectos/:id", (req,...
 // Por: routerProyectos.get("/:id", (req,...
@@ -132,3 +133,56 @@ res.send(datos);
 // tambien, cuando quiera asegurarme de que algo vaya en formato json, uso
 res.json(datos)
 //que lo que hace, es llamar detras de escena a json.stringify
+
+// 3-11-22 *************
+
+//HTML
+
+//https://developer.mozilla.org/es/
+//Elementos: componentes basicos de HTML. Se determinan con las etiquetas/Tags
+//identacion recomendada, 2 espacios. Si el codigo ya fue escrito, Ctrl + ] ó Ctrl + [
+// Alt + Z adapta a que todo el codigo se vea sin tener que desplazarse laterlamente
+//<!---->
+/*seleccionando codigo Ctrl + / lo comenta .. No funciona
+Ir a Preferences->Key Bindings - User y pegar lo siguiente:
+{ "keys": ["ctrl+7"], "command": "toggle_comment", "args": { "block": false } },
+{ "keys": ["ctrl+shift+7"], "command": "toggle_comment", "args": { "block": true } }
+Para probarlo, selecciona el texto, y presiona simultáneamente Ctrl7. Y listo, a disfrutar! La solución fue reemplazar el métodos abreviados de teclado por defecto Ctrl/. Por Ctrl7, que es la misma tecla en el teclado.*/
+//<a href="https://translate.google.com.ar/" target="_blank" rel="nooper noreferrer">acordarse tablero React</a> Para que el enlace abra otra pestaña y por motivos de seguridad (para prevenir un ataque donde la pestaña de origen se reemplaza, para que le usuario deje sus datos, lo cual es una brecha de seguridad)
+/*Enlaces a otros lugares de la misma pagina:
+<a href="#prueba">Prueba</a>
+<h4 id="prueba">Prueba</h4>*/
+//Dead Links href="#" se usan durante el desarrollo, para ver como quedan, antes de direccionar. 
+// Listas No ordenadas:   <ul> <li>1</li> <li>2</li> </ul>
+// Ordenadas: Idem con <ol>
+//<strong>Prueba</strong> <s>Prueba</s> <em>Prueba</em> <small>Prueba</small>
+//dividir secciones: <hr>
+/*Formularios
+<form action="/enviar-respuesta">
+      <label for="masculino"><input id="masculino" type="radio" name="masculino-femenino"
+          value="masculino" />Masculino</label><br />
+
+      <label for="femenino"><input id="femenino" type="radio" name="masculino-femenino" value="femenino"
+          checked />Femenino</label><br />
+
+      <label for="ansioso"><input type="checkbox" id="ansioso" name="personalidad"
+          value="ansioso" />Ansioso</label><br />
+      <label for="perezoso"><input type="checkbox" id="perezoso" name="personalidad" value="perezoso"
+          checked />Perezoso</label><br />
+
+      <input type="text" placeholder="Contraseña" required />
+      <button type="submit">Enviar</button>
+    </form>
+
+-Label hace que al hacer click en el texto al lado del circulito, este se seleccione, lo cual es mas comodo para el usuario
+-For="" la vincula al input con el id correspondiente
+-name hace referencia al grupo (todos los que tengan el mismo name)de ellos, solo uno podra quedar seleccionado
+-Salto de linea: <br />
+-Casillas de Verificacion, a igual name, mismo grupo, pero varias simultaneas a diferencia de radio
+-Checked Marca por defecto    
+-value el valor que tomara el formulario cuando se envie
+*/    
+//div - cajita para contener elementos
+//head - elementos detras de escena. Importantes para el ranking
+
+//CSS
